529c62873b7c4fb085fb201ae795d53cc35974b0
Semyon Kovardaev <s.kovardaev@gmail.com>
2020-10-12 13:46:00 +0300

    delete folders

--- docs/js/__tests__/basic.test.js
+++
@@ -1,6 +0,0 @@
-import sum from '../basic';
-
-test('should sum', () => { //eslint-disable-line
-  const result = sum([1, 2, 3]);
-  expect(result).toBe(6); //eslint-disable-line
-});
--- docs/js/basic.js
+++
@@ -1,7 +0,0 @@
-export default function sum(items) {
-  let result = 0;
-  for (const item of items) { //eslint-disable-line
-    result += item;
-  }
-  return result;
-}
